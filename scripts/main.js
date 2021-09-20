95e0bed019cefa35915934ee347cc893541de74c
Manu <gmanu21097@gmail.com>
2021-09-20 23:57:38 +0530

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,41 @@ function fun4(){
     d3.style.display="none";
     d4.style.display="block";
 }
+
+function fun5(){
+    console.log("asjfhk")
+    var d1=document.getElementById("timeDiv");
+    var dt = Date();
+    dt = dt.substring(0,25);
+    console.log(dt);
+    d1.innerHTML = "Time: " + dt;
+}
+
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+  
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+  
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+    let currentTime = hour + ":" 
+            + min + ":" + sec + " " + am_pm;
+  
+    document.getElementById("clock")
+            .innerHTML = currentTime;
+}
+showTime();
