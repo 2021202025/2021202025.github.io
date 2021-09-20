7e0aed028238ee8ccd67a7e913aa882b6b0de6a0
Manu <gmanu21097@gmail.com>
2021-09-20 22:28:58 +0530

    Update

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+function fun1(){
+    var d1=document.getElementById("profileContent");
+    var d2=document.getElementById("workContent");
+    var d3=document.getElementById("projectsContent");
+    var d4=document.getElementById("educationContent");
+    d1.style.display="block";
+    d2.style.display="none";
+    d3.style.display="none";
+    d4.style.display="none";
+}
+
+function fun2(){
+    var d1=document.getElementById("profileContent");
+    var d2=document.getElementById("workContent");
+    var d3=document.getElementById("projectsContent");
+    var d4=document.getElementById("educationContent");
+    d1.style.display="none";
+    d2.style.display="block";
+    d3.style.display="none";
+    d4.style.display="none";
+}
+
+function fun3(){
+    var d1=document.getElementById("profileContent");
+    var d2=document.getElementById("workContent");
+    var d3=document.getElementById("projectsContent");
+    var d4=document.getElementById("educationContent");
+    d1.style.display="none";
+    d2.style.display="none";
+    d3.style.display="block";
+    d4.style.display="none";
+}
+
+function fun4(){
+    var d1=document.getElementById("profileContent");
+    var d2=document.getElementById("workContent");
+    var d3=document.getElementById("projectsContent");
+    var d4=document.getElementById("educationContent");
+    d1.style.display="none";
+    d2.style.display="none";
+    d3.style.display="none";
+    d4.style.display="block";
+}
