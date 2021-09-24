40f57643b243b2bab6e1abccdb2fe5c8ca0e1c28
Manu <gmanu21097@gmail.com>
2021-09-24 13:43:16 +0530

    Final Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,65 @@
-function fun1(){
-    var d1=document.getElementById("profileContent");
-    var d2=document.getElementById("workContent");
-    var d3=document.getElementById("projectsContent");
-    var d4=document.getElementById("educationContent");
-    d1.style.display="block";
-    d2.style.display="none";
-    d3.style.display="none";
-    d4.style.display="none";
+function fun1() {
+    var d1 = document.getElementById("profileContent");
+    var d2 = document.getElementById("workContent");
+    var d3 = document.getElementById("projectsContent");
+    var d4 = document.getElementById("educationContent");
+    d1.style.display = "block";
+    d2.style.display = "none";
+    d3.style.display = "none";
+    d4.style.display = "none";
 }
 
-function fun2(){
-    var d1=document.getElementById("profileContent");
-    var d2=document.getElementById("workContent");
-    var d3=document.getElementById("projectsContent");
-    var d4=document.getElementById("educationContent");
-    d1.style.display="none";
-    d2.style.display="block";
-    d3.style.display="none";
-    d4.style.display="none";
+function fun2() {
+    var d1 = document.getElementById("profileContent");
+    var d2 = document.getElementById("workContent");
+    var d3 = document.getElementById("projectsContent");
+    var d4 = document.getElementById("educationContent");
+    d1.style.display = "none";
+    d2.style.display = "block";
+    d3.style.display = "none";
+    d4.style.display = "none";
 }
 
-function fun3(){
-    var d1=document.getElementById("profileContent");
-    var d2=document.getElementById("workContent");
-    var d3=document.getElementById("projectsContent");
-    var d4=document.getElementById("educationContent");
-    d1.style.display="none";
-    d2.style.display="none";
-    d3.style.display="block";
-    d4.style.display="none";
+function fun3() {
+    var d1 = document.getElementById("profileContent");
+    var d2 = document.getElementById("workContent");
+    var d3 = document.getElementById("projectsContent");
+    var d4 = document.getElementById("educationContent");
+    d1.style.display = "none";
+    d2.style.display = "none";
+    d3.style.display = "block";
+    d4.style.display = "none";
 }
 
-function fun4(){
-    var d1=document.getElementById("profileContent");
-    var d2=document.getElementById("workContent");
-    var d3=document.getElementById("projectsContent");
-    var d4=document.getElementById("educationContent");
-    d1.style.display="none";
-    d2.style.display="none";
-    d3.style.display="none";
-    d4.style.display="block";
+function fun4() {
+    var d1 = document.getElementById("profileContent");
+    var d2 = document.getElementById("workContent");
+    var d3 = document.getElementById("projectsContent");
+    var d4 = document.getElementById("educationContent");
+    d1.style.display = "none";
+    d2.style.display = "none";
+    d3.style.display = "none";
+    d4.style.display = "block";
 }
 
-function fun5(){
+function fun5() {
     console.log("asjfhk")
-    var d1=document.getElementById("timeDiv");
+    var d1 = document.getElementById("timeDiv");
     var dt = Date();
-    dt = dt.substring(0,25);
+    dt = dt.substring(0, 25);
     console.log(dt);
     d1.innerHTML = "Time: " + dt;
 }
 
 setInterval(showTime, 1000);
+
 function showTime() {
     let time = new Date();
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
     am_pm = "AM";
-  
+
     if (hour > 12) {
         hour -= 12;
         am_pm = "PM";
@@ -67,15 +68,24 @@ function showTime() {
         hr = 12;
         am_pm = "AM";
     }
-  
+
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    console.log(today);
+
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
-  
-    let currentTime = hour + ":" 
-            + min + ":" + sec + " " + am_pm;
-  
+
+    let currentTime = hour + ":" +
+        min + ":" + sec + " " + am_pm;
+
     document.getElementById("clock")
-            .innerHTML = currentTime;
+        .innerHTML = today + "  " + currentTime;
 }
 showTime();
